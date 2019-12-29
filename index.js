3df7d4164ef56890a3e7117d28528abdd7087276
Johannes Bauer <52888466+Joh4nnesB@users.noreply.github.com>
2019-12-29 18:23:08 +0100

    Implemented world editing

--- index.js
+++ index.js
@@ -4,15 +4,41 @@ const BLOCK_OUTLINE = BLOCK_SIZE + BLOCK_GAP;
 
 class Block {
   hovered = false;
+  wall = false;
+  spawn = false;
+  target = false;
 
   constructor(x, y) {
     this.x = x;
     this.y = y;
   }
+
+  getColor() {
+    if (this.wall) {
+      return "blue";
+    } else if (this.spawn) {
+      return "yellow";
+    } else if (this.target) {
+      return "green";
+    } else {
+      return "white";
+    }
+  }
 }
 
 class World {
   blocks = [];
+
+  /**
+   * 0 = no mouse button down
+   * 1 = left mouse button down
+   * 3 = right mouse button down
+   */
+  currentMouseButton = 0;
+
+  placeWalls = true;
+  placeSpawn = false;
+  placeTarget = false;
 
   constructor(sizeX, sizeY) {
     this.sizeX = sizeX;
@@ -21,7 +47,8 @@ class World {
     this.fillWorld();
     this.setupCanvas();
     this.render();
-    this.addHoverListener();
+    this.addMouseMoveListener();
+    this.addMouseClickListener();
   }
 
   /**
@@ -59,7 +86,7 @@ class World {
 
     // Draw a square for each block
     this.blocks.flat().forEach(block => {
-      this.context.fillStyle = "white";
+      this.context.fillStyle = block.getColor();
       // If the block is hovered, the square will be bigger than normal
       if (block.hovered) {
         this.context.fillRect(
@@ -77,6 +104,8 @@ class World {
         );
       }
     });
+
+    requestAnimationFrame(_ => this.render());
   }
 
   /**
@@ -98,27 +127,78 @@ class World {
     return this.blocks[y][x];
   }
 
+  setSpawn() {
+    this.placeWalls = false;
+    this.placeSpawn = true;
+    this.placeTarget = true;
+  }
+
+  setTarget() {
+    this.placeWalls = false;
+    this.placeSpawn = false;
+    this.placeTarget = true;
+  }
+
+  worldSetupCompleted() {
+    this.placeWalls = false;
+    this.placeSpawn = false;
+    this.placeTarget = false;
+  }
+
   /**
-   * Checks if a block is hovered and re-renders the world each time the mouse was moved
+   * @param {MouseEvent} event
    */
-  addHoverListener() {
-    let lastBlock;
+  onMouseUpdate(event) {
+    const canvasBoundingRect = this.canvas.getBoundingClientRect();
+    const currentBlock = this.raycastBlock(
+      event.clientX - canvasBoundingRect.x,
+      event.clientY - canvasBoundingRect.y
+    );
 
-    window.onmousemove = event => {
-      const canvasBoundingRect = this.canvas.getBoundingClientRect();
-      const currentBlock = this.raycastBlock(
-        event.clientX - canvasBoundingRect.x,
-        event.clientY - canvasBoundingRect.y
-      );
-
-      if (lastBlock) lastBlock.hovered = false;
-      if (currentBlock) {
-        currentBlock.hovered = true;
-        lastBlock = currentBlock;
+    if (this.lastBlock) this.lastBlock.hovered = false;
+    if (currentBlock) {
+      if (this.placeWalls) {
+        if (this.currentMouseButton === 1) {
+          currentBlock.wall = true;
+        } else if (this.currentMouseButton === 3) {
+          currentBlock.wall = false;
+        }
+      } else if (this.currentMouseButton === 1 && !currentBlock.wall) {
+        if (this.placeSpawn) {
+          currentBlock.spawn = true;
+          this.setTarget();
+        } else if (this.placeTarget && !currentBlock.spawn) {
+          currentBlock.target = true;
+          this.worldSetupCompleted();
+        }
       }
 
-      this.render();
+      currentBlock.hovered = true;
+      this.lastBlock = currentBlock;
+    }
+  }
+
+  addMouseMoveListener() {
+    window.onmousemove = event => this.onMouseUpdate(event);
+  }
+
+  addMouseClickListener() {
+    window.onmousedown = event => {
+      this.currentMouseButton = event.which;
+      this.onMouseUpdate(event);
     };
+
+    // Set the current mouse button to 0 when the mouse button was released
+    window.onmouseup = event => {
+      if (this.currentMouseButton === event.which) {
+        this.currentMouseButton = 0;
+      }
+    };
+
+    window.onblur = _ => (this.currentMouseButton = 0);
+
+    // Don't open the context menu on the canvas
+    this.canvas.oncontextmenu = event => event.preventDefault();
   }
 }
 
